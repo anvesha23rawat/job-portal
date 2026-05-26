4184171184ddd9cce780d70dd6e64bb4ac53dee8
r-srishti <srishtirawat859@gmail.com>
2026-05-26 21:03:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
